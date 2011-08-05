3e2760d553d2d43e7d46a7aa8a728506d9509798
Christian Horsdal <c.horsdal@gmail.com>
2011-08-06 01:44:49 +0200

    adding support for creating new random week plan

---
+++ �velsesPlan/Helpers/ListHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+ 
+namespace ØvelsesPlan.Helpers
+{
+    public static class ListHelpers
+    {
+        private static int seed = (int) (DateTime.Now.Ticks % 1009); 
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> enumeration)
+        {
+            var asList = enumeration.ToList();
+            var length = asList.Count();
+            var indexes = Enumerable.Range(0, length).ToArray();
+            var generator = new Random(seed++);
+
+            for (int i = 0; i < length; ++i)
+            {
+                var position = generator.Next(i, length);
+
+                yield return asList[indexes[position]];
+
+                indexes[position] = indexes[i];
+            }
+        }
+    }
+}
