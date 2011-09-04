2ddaa85fe6491512668a7a05d4d89e94c0fadd6f
Christian Horsdal <c.horsdal@gmail.com>
2011-09-04 23:55:27 +0200

    adding first little test of Nancy module

---
+++ �velsesPlanTests/WebApiSpec.cs
@@ -0,0 +1,45 @@
+﻿using Nancy;
+using Nancy.Testing;
+using Should.Fluent;
+using Xunit;
+using Specs;
+using ØvelsesPlan;
+
+namespace ØvelsesPlanTests
+{
+    public class WebApiSpec : MongoIntegrationSpec
+    {
+        private Browser app;
+
+        public WebApiSpec()
+        {
+            ExercisePlannerModule artificialReference;
+            var bootstrapper = new DefaultNancyBootstrapper();
+            bootstrapper.Initialise();
+            app = new Browser(bootstrapper);
+        }
+
+        [Fact]
+        public void when_call_base_url()
+        {
+
+            var response = app.Get("/", with => with.HttpRequest());
+
+            "the app".should
+                (() =>
+                    {
+                        "return the index page".asIn
+                            (
+                                () => response.StatusCode.Should().Equal(HttpStatusCode.OK)
+                            ).andIn(
+                                () => response.Body["title"].ShouldContain("Øvelsesplan")
+                            );
+                        "have retrieved the current week plan".asIn
+                            (
+                                () => Assert.True(true) //TDB  
+                            );
+                    }
+                );
+        }
+    }
+}
