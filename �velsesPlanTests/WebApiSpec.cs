3d474f4936c6c3ff43d8b4cf00b56b33c264b514
Christian Horsdal <c.horsdal@gmail.com>
2011-09-06 12:27:47 +0200

    adding a couple of module tests more

--- �velsesPlanTests/WebApiSpec.cs
+++ �velsesPlanTests/WebApiSpec.cs
@@ -20,26 +20,65 @@ namespace ØvelsesPlanTests
         }
 
         [Fact]
-        public void when_call_base_url()
+        public void when_calling_base_url()
         {
-
             var response = app.Get("/", with => with.HttpRequest());
 
             "the app".should
                 (() =>
-                    {
                         "return the index page".asIn
                             (
                                 () => response.StatusCode.Should().Equal(HttpStatusCode.OK)
                             ).andIn(
-                                () => response.Body["title"].ShouldContain("Øvelsesplan")
-                            );
-                        "have retrieved the current week plan".asIn
-                            (
-                                () => Assert.True(true) //TDB  
-                            );
-                    }
+                                () =>
+                                    {
+                                        response.Body["title"].ShouldContain("Øvelsesplan");
+                                  //      response.Body["#exercises"].ShouldExistOnce();
+                                    })
                 );
+        }
+
+        [Fact]
+        public void when_calling_exercises()
+        {
+            var response = app.Get("/exercises/", with => with.HttpRequest());
+
+            "the app".should
+                (() =>
+                    "return the exercises page".asIn
+                        (
+                            () => response.StatusCode.Should().Equal(HttpStatusCode.OK)
+                        ).andIn(
+                            () =>
+                            {
+                                response.Body["title"].ShouldContain("Øvelser");
+                                //response.Body["#exercises"].ShouldExistOnce();
+                            })
+                );
+        }
+
+        [Fact]
+        public void when_calling_exercises_all()
+        {
+            var response = app.Get("/exercises/all/", with => with.HttpRequest());
+
+            "the app".should
+                (() =>
+                {
+                    "return json".asIn
+                        (
+                            () => response.StatusCode.Should().Equal(HttpStatusCode.OK)
+                        ).andIn(
+                            () =>
+                                {
+                                    response.Context.Response.ContentType.Should().Contain("json");
+                                    //response.Body["#exercises"].ShouldExistOnce();
+                                });
+                    "have retrieved all exercises".asIn
+                        (
+                            () => Assert.True(true) //TDB  
+                        );
+                });
         }
     }
 }
